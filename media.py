30af1f4ef4798004e1855f45baec686bc852b7a4
zdburrage <zdburrage@crimson.ua.edu>
2017-09-27 20:47:42 -0500

    Final commit after PEP-8 changes

--- media.py
+++ media.py
@@ -1,21 +1,29 @@
 import webbrowser
-# Parnet Class
+# Parent Class
+
+
 class Video():
-    def __init__(self,title,duration):
+    def __init__(self, title, duration):
         self.title = title
         self.duration = duration
 # Child of Video
+
+
 class TvShow(Video):
-    def __init__(self,title,duration,season,episode):
+    def __init__(self, title, duration, season, episode):
         Video.__init__(self, title, duration)
         self.season = season
         self.episode = episode
-#Child of Video and main class used for this project
+# Child of Video and main class used for this project
+
+
 class Movie(Video):
-    def __init__(self,title, duration,movie_storyline,poster_image,trailer_youtube):
-        Video.__init__(self, title,duration)
+    def __init__(self, title, duration, movie_storyline,
+                 poster_image, trailer_youtube):
+        Video.__init__(self, title, duration)
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
