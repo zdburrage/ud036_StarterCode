75dedc9d0151c524e82025f79e5d4c2345b2e0ec
zdburrage <zdburrage@crimson.ua.edu>
2017-09-27 20:20:18 -0500

    Final Commit

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,7 @@
 import media
 import fresh_tomatoes
 
-
+# Constructing each movie
 toy_story = media.Movie("Toy Story","1:21:00","A story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
 "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 superbad = media.Movie("Superbad","1:53:00","Two friends go on a quest to have an awesome time at a party before they graduate","http://www.cinemasterpieces.com/superbadjan08.jpg","https://www.youtube.com/watch?v=G05Xdl5eGqo")
@@ -11,8 +11,9 @@ the_big_short = media.Movie("The Big Short","2:10:00","Three groups of people di
                             "http://cdn.film-book.com/wp-content/uploads/2015/11/the-big-short-movie-poster-01.png","https://www.youtube.com/watch?v=vgqG3ITMv1Q")
 a_walk_to_remember = media.Movie("A Walk to Remember","1:42:00","A jaded, aimless high school senior (Shane West) who falls in love with a guileless young woman (Mandy Moore) he and his friends once scorned"
                                  ,"https://images-na.ssl-images-amazon.com/images/I/51TKV2HF1TL._SY445_.jpg","https://www.youtube.com/watch?v=EgdoQ8Oxu2E")
-
+# Gathering movies in a list
 movies = [toy_story,superbad,django,ily_man,the_big_short,a_walk_to_remember]
 
+# Generate HTML page from function in fresh_tomatoes.py
 fresh_tomatoes.open_movies_page(movies)
 
--- media.py
+++ media.py
@@ -1,15 +1,16 @@
 import webbrowser
-
+# Parnet Class
 class Video():
     def __init__(self,title,duration):
         self.title = title
         self.duration = duration
-
+# Child of Video
 class TvShow(Video):
-    def __init__(self,season,episode):
+    def __init__(self,title,duration,season,episode):
+        Video.__init__(self, title, duration)
         self.season = season
         self.episode = episode
-
+#Child of Video and main class used for this project
 class Movie(Video):
     def __init__(self,title, duration,movie_storyline,poster_image,trailer_youtube):
         Video.__init__(self, title,duration)
