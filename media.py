0b1c0d18e2cee2dae03ae20d22564ca0ea605bbb
zdburrage <zdburrage@crimson.ua.edu>
2017-09-28 10:12:28 -0500

    Update media.py

--- media.py
+++ media.py
@@ -1,23 +1,36 @@
 import webbrowser
-# Parent Class
 
 
 class Video():
+    """The Parent class for Movie and TvShow. This hold the properties
+    and initialization function for title and duration. Takes two
+    arguments to initialize (title and duration) both of type
+    string and can be called from a child class"""
     def __init__(self, title, duration):
         self.title = title
         self.duration = duration
-# Child of Video
 
 
 class TvShow(Video):
+    """This is a child class of Video that inherits its properties title
+    and duration. Its __init__ function calls the Video __init__ function
+    which sets the title and duration. Its(TvShow's) __init__ function also sets
+    the properties season and episode for the object equal to the
+    values passed in"""
     def __init__(self, title, duration, season, episode):
         Video.__init__(self, title, duration)
         self.season = season
         self.episode = episode
-# Child of Video and main class used for this project
 
 
 class Movie(Video):
+    """The main class used for this project and a child class of video.
+    Inherits the properties title and duration from Video. Its
+    __init__ function calls Video's __init__ function to set
+    the above mentioned properties. The other peoperties passed in
+    (movie_storyline, poster_image, and trailer_youtube) are
+    set below the video __init__ call, made equal to the values
+    passed in."""
     def __init__(self, title, duration, movie_storyline,
                  poster_image, trailer_youtube):
         Video.__init__(self, title, duration)
@@ -26,4 +39,9 @@ class Movie(Video):
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """This function uses the webbrowser library to access
+        a web page with the object's current value in the
+        trailer_youtube_url field, which would ideally
+        go to youtube and play the trailer of the
+        desired movie"""
         webbrowser.open(self.trailer_youtube_url)
